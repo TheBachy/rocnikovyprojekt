0a5eec9587db2d0ba95091999124c57226171070
TheBachy <60212534+TheBachy@users.noreply.github.com>
2021-04-13 22:01:31 +0200

    Pridani zalozek do admin stranky

--- RLReplays/admin.py
+++ RLReplays/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Players)
+admin.site.register(PlayersHasReplay)
+admin.site.register(Replay)
+admin.site.register(ReplayHasTeams)
+admin.site.register(Teams)
+
 # Register your models here.
